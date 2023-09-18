55c76830b95467a39aeb68c850387be14a98c749
MHP24 <mig.henriquezp@gmail.com>
2023-09-18 13:06:48 -0300

    update: Navbar (add labels on hover)

--- src/app/components/ui/NavItem.tsx
+++ src/app/components/ui/NavItem.tsx
@@ -1,6 +1,8 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 type Props = {
   path: string,
@@ -8,14 +10,30 @@ type Props = {
 }
 
 export const NavItem: FC<Props> = ({ path, asset }) => {
+
+  const [isHover, setIsHover] = useState<boolean>(false);
+
   return (
-    <Link href={path}
-      className='grid place-items-center m-auto'
-    >
-      <Image 
-        className='bg-c6 transition-all delay-75 hover:opacity-80 p-3 rounded-full border-[1px] border-c3-1'
-        src={`/svgs/${asset}.svg`} alt={path} width={54} height={54}
-      />
-    </Link>
+    <div className='relative flex items-center'>
+      <Link href={path}
+        className='grid place-items-center m-auto'
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        <Image 
+          className='bg-c6 transition-all delay-75 hover:opacity-80 p-3 rounded-full border-[1px] border-c3-1'
+          src={`/svgs/${asset}.svg`} alt={path} width={54} height={54}
+        />
+      </Link>
+
+      {
+        isHover && (
+          <p 
+            className='absolute translate-x-16 capitalize text-sm bg-c6 px-2 py-1 rounded-md border-[1px] border-c3-1'>
+            {path}
+          </p>
+        )
+      }
+    </div>
   );
 };
